7fb4000c01c843a55128726d64660fb0689903a8
Jeff Huth <39202799+jeffhuth-bytecode@users.noreply.github.com>
2019-09-05 08:40:21 -0700

    v.0.0.3 fix sync datetime conversion (#2)
    v.0.0.3 fix sync datetime conversion

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@
 from setuptools import setup, find_packages
 
 setup(name='tap-linkedin-ads',
-      version='0.0.2',
+      version='0.0.3',
       description='Singer.io tap for extracting data from the LinkedIn Marketing Ads API API 2.0',
       author='jeff.huth@bytecode.io',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_linkedin_ads'],
       install_requires=[
           'backoff==1.8.0',
-          'requests==2.20.0',
-          'singer-python==5.8.0'
+          'requests==2.22.0',
+          'singer-python==5.8.1'
       ],
       entry_points='''
           [console_scripts]
