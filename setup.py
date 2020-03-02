3aa36139fab9996a41fe7255f01a9ba7b6126819
zozorleponey <emile.caron@uptilab.com>
2020-03-02 15:33:55 +0100

    downgrade requests minimal version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(name='tap-linkedin-ads',
       py_modules=['tap_linkedin_ads'],
       install_requires=[
           'backoff>=1.8.0',
-          'requests>=2.22.0',
+          'requests>=2.21.0',
           'singer-python>=5.8.1'
       ],
       extras_require={
