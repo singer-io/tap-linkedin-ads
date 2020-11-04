4895d055b1f0f7a11296248276a89ba3515060d7
Zach Harris <zharris@talend.com>
2020-11-04 17:10:32 +0000

    Bump to v1.0.2, update changelog

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='tap-linkedin-ads',
-      version='1.0.1',
+      version='1.0.2',
       description='Singer.io tap for extracting data from the LinkedIn Marketing Ads API API 2.0',
       author='jeff.huth@bytecode.io',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
