896b5f08c4d5090d8618c8657d0a71218a4dc0ea
savan-chovatiya <80703490+savan-chovatiya@users.noreply.github.com>
2021-08-13 00:01:40 +0530

    TDL-14567: Check linkedin ads accounts in discover mode (#35)
    * TDL-14567: Add check for valid linkedin ads accounts in discover mode
    
    * TDL-14567: Resolved pylint failure
    
    * TDL-14567: Resolved unit test failure
    
    * Added valid number format check in API call handling for accou
    
    Co-authored-by: Harsh <80324346+harshpatel4crest@users.noreply.github.com>

--- tap_linkedin_ads/__init__.py
+++ tap_linkedin_ads/__init__.py
@@ -6,7 +6,7 @@ import argparse
 import singer
 from singer import metadata, utils
 from tap_linkedin_ads.client import LinkedinClient
-from tap_linkedin_ads.discover import discover, check_accounts_list
+from tap_linkedin_ads.discover import discover
 from tap_linkedin_ads.sync import sync as _sync
 
 LOGGER = singer.get_logger()
@@ -18,10 +18,10 @@ REQUIRED_CONFIG_KEYS = [
     'accounts'
 ]
 
-def do_discover(config):
+def do_discover(client, config):
 
     LOGGER.info('Starting discover')
-    check_accounts_list(config)
+    client.check_accounts(config)
     catalog = discover()
     json.dump(catalog.to_dict(), sys.stdout, indent=2)
     LOGGER.info('Finished discover')
@@ -39,7 +39,7 @@ def main():
             state = parsed_args.state
 
         if parsed_args.discover:
-            do_discover(parsed_args.config)
+            do_discover(client, parsed_args.config)
         elif parsed_args.catalog:
             _sync(client=client,
                   config=parsed_args.config,
