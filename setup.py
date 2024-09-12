89d08243917628d1f9da090d00266834f5c4a710
Bryant Gray <bgray@talend.com>
2024-09-12 18:24:59 +0000

    bump requests version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(name='tap-linkedin-ads',
       py_modules=['tap_linkedin_ads'],
       install_requires=[
           'backoff==1.8.0',
-          'requests==2.22.0',
+          'requests==2.32.3',
           'singer-python==5.12.1'
       ],
       extras_require={
