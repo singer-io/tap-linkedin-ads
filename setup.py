eddd376fdc49b8b8024b7fcef774e54d87f3da4e
Rutuja Deshmukh <rutuja.deshmukh@qlik.com>
2025-02-12 04:14:38 +0000

    update backoff version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(name='tap-linkedin-ads',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_linkedin_ads'],
       install_requires=[
-          'backoff==1.8.0',
+          'backoff==2.2.1',
           'requests==2.32.3',
           'singer-python==6.0.0'
       ],
