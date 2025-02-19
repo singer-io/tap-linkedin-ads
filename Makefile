b49ffc25695e3fd40f49e115fcbc9514fc4f46af
Rutuja Deshmukh <107538720+rdeshmukh15@users.noreply.github.com>
2025-02-19 15:11:27 +0530

    api version upgrade 202501 (#76)
    * api version update changes
    
    * update backoff version
    
    * changes in CHANGELOG.md
    
    * removes Makefile

--- Makefile
+++
@@ -1,4 +0,0 @@
-.DEFAULT_GOAL := lint
-
-lint:
-	pylint tap_linkedin_ads -d 'broad-except,line-too-long,missing-class-docstring,missing-function-docstring,missing-module-docstring,too-many-locals,invalid-name,consider-using-f-string,use-list-literal,use-dict-literal,raise-missing-from, unspecified-encoding, broad-exception-raised'
--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@
 from setuptools import setup, find_packages
 
 setup(name='tap-linkedin-ads',
-      version='2.3.1',
+      version='2.4.0',
       description='Singer.io tap for extracting data from the LinkedIn Marketing Ads API API 2.0',
       author='jeff.huth@bytecode.io',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_linkedin_ads'],
       install_requires=[
-          'backoff==1.8.0',
-          'requests==2.22.0',
-          'singer-python==5.12.1'
+          'backoff==2.2.1',
+          'requests==2.32.3',
+          'singer-python==6.1.0'
       ],
       extras_require={
         'dev': [
