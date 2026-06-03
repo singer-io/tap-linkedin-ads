e1cecf0f3f5dfeda120329ffc4420f114aeebdc8
Leslie VanDeMark <38043390+leslievandemark@users.noreply.github.com>
2026-06-03 16:55:47 -0400

    bump requests 2.33.0 (#85)

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from setuptools import setup, find_packages
 
 setup(name='tap-linkedin-ads',
-      version='2.6.0',
+      version='2.6.1',
       description='Singer.io tap for extracting data from the LinkedIn Marketing Ads API API 2.0',
       author='jeff.huth@bytecode.io',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_linkedin_ads'],
       install_requires=[
           'backoff==2.2.1',
-          'requests==2.32.5',
+          'requests==2.33.0',
           'singer-python==6.3.0'
       ],
       extras_require={
